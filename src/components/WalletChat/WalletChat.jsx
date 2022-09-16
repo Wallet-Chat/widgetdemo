219eee445b98b16cd531721ecc1edfef704f713a
Randell Crapy <randell.crapy@gmail.com>
2022-09-16 11:36:46 +0800

    Added a button that can open it up like superare

--- src/components/WalletChat/WalletChat.jsx
+++ src/components/WalletChat/WalletChat.jsx
@@ -1,9 +1,19 @@
 
+import React, { useState } from 'react';
+import ButtonOverlay from "../ButtonOverlay/ButtonOverlay" 
 import "./WalletChat.css"
 export default function WalletChatWidget(){
+    const [isOpen, setIsOpen] = useState(false);
+    const clickHandler = (e) =>{
+        setIsOpen(!isOpen)
+    }
     return (
-        <>
-            <iframe id="wallet-chat-widget" src="http://localhost:3000"></iframe>
-        </>
+        <div id="wallet-chat-widget__container">
+            {isOpen && (
+                <iframe id="wallet-chat-widget" src="http://localhost:3000"></iframe>
+            )}
+            
+            <ButtonOverlay isOpen={isOpen} clickHandler={clickHandler}/>
+        </div>
     )
 }
