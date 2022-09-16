a9a19ea4cb169443ad1ab611ba733d4343334274
Randell Crapy <randell.crapy@gmail.com>
2022-09-16 09:43:01 +0800

    Added example widget integration

---
+++ src/components/WalletChat/WalletChat.jsx
@@ -0,0 +1,9 @@
+
+import "./WalletChat.css"
+export default function WalletChatWidget(){
+    return (
+        <>
+            <iframe id="wallet-chat-widget" src="http://localhost:3000"></iframe>
+        </>
+    )
+}
