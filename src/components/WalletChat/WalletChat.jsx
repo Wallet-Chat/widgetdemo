38e950c80310ec949cc8b963dd43b83258d2e25f
Cyber Kevin <walletchatextension@gmail.com>
2022-09-15 20:16:27 -0700

    using live site

--- src/components/WalletChat/WalletChat.jsx
+++ src/components/WalletChat/WalletChat.jsx
@@ -3,7 +3,7 @@ import "./WalletChat.css"
 export default function WalletChatWidget(){
     return (
         <>
-            <iframe id="wallet-chat-widget" src="http://localhost:3000"></iframe>
+            <iframe id="wallet-chat-widget" src="https://app.walletchat.fun/"></iframe>
         </>
     )
 }
