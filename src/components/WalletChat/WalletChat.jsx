bd8ac3dcd7cc2dd227f4a97461d89fa09af2294a
Randell Crapy <randell.crapy@gmail.com>
2022-09-16 12:04:53 +0800

    Made it not refresh everytime popup closes

--- src/components/WalletChat/WalletChat.jsx
+++ src/components/WalletChat/WalletChat.jsx
@@ -4,15 +4,22 @@ import ButtonOverlay from "../ButtonOverlay/ButtonOverlay"
 import "./WalletChat.css"
 export default function WalletChatWidget(){
     const [isOpen, setIsOpen] = useState(false);
+    const url = "https://app.walletchat.fun/"//"app.walletchat.fun" //http://localhost:3000
     const clickHandler = (e) =>{
         setIsOpen(!isOpen)
     }
     return (
         <div id="wallet-chat-widget__container">
-            {isOpen && (
-                <iframe id="wallet-chat-widget" src="http://localhost:3000"></iframe>
-            )}
-            
+            {/* {isOpen && (
+                <iframe id="wallet-chat-widget" src={url}></iframe>
+            )} */}
+            <iframe id="wallet-chat-widget" style={{
+                    height:isOpen?"":"0px",
+                    width:isOpen?"":"0px",
+                    minHeight:isOpen?"":"0px",
+                    minWidth:isOpen?"":"0px"
+                    // display: isOpen?"block":"none"
+                }} src="http://localhost:3000"></iframe>
             <ButtonOverlay isOpen={isOpen} clickHandler={clickHandler}/>
         </div>
     )
